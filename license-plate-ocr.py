1341ea6123b91174b1c890ec43bcdfa2c3dd3315
Ravitej Bhagavathi <30770730+YoursTruely@users.noreply.github.com>
2020-11-16 12:43:35 +0530

    Edited glob argument formatting

--- license-plate-ocr.py
+++ license-plate-ocr.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
 		ocr_net  = dn.load_net(ocr_netcfg.encode('utf-8'), ocr_weights.encode('utf-8'), 0)
 		ocr_meta = dn.load_meta(ocr_dataset.encode('utf-8'))
 
-		imgs_paths = sorted(glob('%s/*lp.png' % output_dir))
+		imgs_paths = sorted(glob(output_dir,'/*lp.png'))
 
 		print ('Performing OCR...')
 
